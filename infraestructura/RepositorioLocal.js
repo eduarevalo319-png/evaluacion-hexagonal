45b57c2dade5a79b109187feb7f80ab8a111f9f4
EDUARDO AREVALO <eduarevalo319@gmail.com>
2026-04-22 16:34:45 -0500

    Cambios realizados

--- infraestructura/RepositorioLocal.js
+++ infraestructura/RepositorioLocal.js
@@ -6,9 +6,6 @@ class RepositorioLocal {
             new Servidor("Cbos. Juan Perez", [15, 16, 14]),
             new Servidor("Cbos. Luis Silva", [12, 10, 11]),
             new Servidor("Cbos. Ana Gomez", [19, 18, 20]),
-            new Servidor("Cbos. Ana Gomez", [19, 18, 20]),
-            new Servidor("Cbos. Ana Gomez", [19, 18, 20]),
-            new Servidor("Cbos. Ana Gomez", [19, 18, 20]),
             new Servidor("Cbos. Carlos Ruiz", [14, 14, 15])
         ];
     }
